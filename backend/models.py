5c58d4e4a365f4dbb857614cd9df2fb1763a317c
beatrice <bbrownmulry@gmail.com>
2025-03-21 15:23:05 -0400

    add secondary pydantic models with relations

--- backend/models.py
+++ backend/models.py
@@ -119,7 +119,7 @@ class UserBase(BaseModel):
         orm_mode: bool = True
 
 
-class EvenBase(BaseModel):
+class EventBase(BaseModel):
     id: int
     name: str
     first_date: str
@@ -149,6 +149,62 @@ class GroupBase(BaseModel):
 
     class Config:
         orm_mode: bool = True
+
+
+# define top-level pydantic classes *with* relations
+# based on the approach used here: https://www.gormanalysis.com/blog/many-to-many-relationships-in-fastapi/
+class User(UserBase):
+    # relationships
+    owned_groups: list[GroupBase]
+    joined_groups: list[GroupBase]
+
+    events: list[EventBase]
+
+    costs: list[CostBase]
+    receipts: list[CostBase]
+
+
+class Event(EventBase):
+    # relationships
+    # user relationship
+    member_ids: list[int]
+    members: list["UserAlchemy"]
+
+    # group relationship
+    group_id: int
+    group: GroupBase
+
+
+class Cost(CostBase):
+    # relationships
+    recipient_id: int
+    recipient: UserBase
+
+    sender_ids: list[int]
+    senders: list[UserBase]
+
+    # group relationship
+    group_id: int
+    group: GroupBase
+
+
+class Group(GroupBase):
+    # relationships
+    # creator relationship
+    creator_id: int
+    creator: UserBase
+
+    # member relationship
+    member_ids: list[int]
+    members: list[UserBase]
+
+    # event relationship
+    event_ids: list[int]
+    events: list[EventBase]
+
+    # cost relationship
+    cost_ids: list[int]
+    costs: list[CostBase]
 
 
 if __name__ == "__main__":
