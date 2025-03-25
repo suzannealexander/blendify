4070b6ca8c3f3e3b251cfa05e3a75fb2ef0027f2
beatrice <bbrownmulry@gmail.com>
2025-03-25 16:48:51 -0400

    add buttons and a searchbar to the nav

--- frontend/components/NavFrame.tsx
+++ frontend/components/NavFrame.tsx
@@ -1,3 +1,33 @@
+import Link from "next/link";
+
+function NavSearchBox() {
+	return (
+		<div className="border-[1px] border-gray-400 bg-gray-50 p-2">
+			<input type="text" placeholder={"Search"} />
+		</div>
+	);
+}
+
+function NavButton({ label, href }: { label: string; href: string }) {
+	return (
+		<Link
+			href={href}
+			className="w-24 border-[1px] border-gray-400 bg-gray-50 p-2 text-center"
+		>
+			{label}
+		</Link>
+	);
+}
+
 export default function NavFrame() {
-	return <div className="border-[1px] border-gray-500 p-2">hello world</div>;
+	return (
+		<div className="m-auto flex h-max w-full max-w-5xl flex-row flex-nowrap items-center justify-between gap-32 p-8">
+			<div className="text-center text-4xl font-semibold">Do & Due</div>
+			<div className="flex flex-row flex-nowrap gap-32">
+				<NavButton label={"Home"} href={""} />
+				<NavButton label={"Profile"} href={""} />
+			</div>
+			<NavSearchBox />
+		</div>
+	);
 }
