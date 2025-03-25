d9c1f86b208aaf27924f7910ff6009add4c48a9c
beatrice <bbrownmulry@gmail.com>
2025-03-25 16:08:16 -0400

    initialize root route components

---
+++ frontend/components/ContentFrame.tsx
@@ -0,0 +1,3 @@
+export default function ContentFrame() {
+	return <div className="border-[1px] border-gray-500 p-2">hello world</div>;
+}
---
+++ frontend/components/NavFrame.tsx
@@ -0,0 +1,3 @@
+export default function NavFrame() {
+	return <div className="border-[1px] border-gray-500 p-2">hello world</div>;
+}
