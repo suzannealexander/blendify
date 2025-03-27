337ab225af6fe510091497310fccfaf0ccd2b8fa
beatrice <bbrownmulry@gmail.com>
2025-03-27 17:26:57 -0400

    add profile picture bubble to nav with locally hosted placeholder (maybe use a smaller image for it?)

--- frontend/components/NavFrame.tsx
+++ frontend/components/NavFrame.tsx
@@ -1,11 +1,24 @@
 import Link from "next/link";
+import Image from "next/image";
 import { JSX } from "react";
+
+function ProfileFrame() {
+	return (
+		<Image
+			src="/profile-placeholder.png"
+			alt="Profile"
+			width={48}
+			height={48}
+			className="cursor-pointer rounded-[50%] object-cover"
+		/>
+	);
+}
 
 function NavButton({ label, href }: { label: string; href: string }) {
 	return (
 		<Link
 			href={href}
-			className="w-24 rounded-lg p-2 text-center text-lg font-[500] hover:bg-gray-50"
+			className="w-24 cursor-pointer rounded-lg p-2 text-center text-lg font-[500] hover:bg-gray-50 hover:text-purple-600"
 		>
 			{label}
 		</Link>
@@ -26,6 +39,7 @@ export default function NavFrame() {
 			strokeWidth="2"
 			strokeLinecap="round"
 			strokeLinejoin="round"
+			className="cursor-pointer stroke-black hover:stroke-purple-600"
 		>
 			<circle cx="11" cy="11" r="8" />
 			<path d="m21 21-4.3-4.3" />
@@ -42,6 +56,7 @@ export default function NavFrame() {
 			strokeWidth="2"
 			strokeLinecap="round"
 			strokeLinejoin="round"
+			className="cursor-pointer stroke-black hover:stroke-purple-600"
 		>
 			<path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
 			<circle cx="12" cy="12" r="3" />
@@ -58,6 +73,7 @@ export default function NavFrame() {
 			strokeWidth="2"
 			strokeLinecap="round"
 			strokeLinejoin="round"
+			className="cursor-pointer stroke-black hover:stroke-purple-600"
 		>
 			<path d="M10.268 21a2 2 0 0 0 3.464 0" />
 			<path d="M3.262 15.326A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673C19.41 13.956 18 12.499 18 8A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.738 7.326" />
@@ -74,6 +90,7 @@ export default function NavFrame() {
 			strokeWidth="2"
 			strokeLinecap="round"
 			strokeLinejoin="round"
+			className="cursor-pointer stroke-black hover:stroke-purple-600"
 		>
 			<path d="M10.268 21a2 2 0 0 0 3.464 0" />
 			<path d="M13.916 2.314A6 6 0 0 0 6 8c0 4.499-1.411 5.956-2.74 7.327A1 1 0 0 0 4 17h16a1 1 0 0 0 .74-1.673 9 9 0 0 1-.585-.665" />
@@ -98,7 +115,7 @@ export default function NavFrame() {
 					strokeWidth="1"
 					strokeLinecap="round"
 					strokeLinejoin="round"
-					className="stroke-black hover:stroke-purple-600"
+					className="cursor-pointer stroke-black hover:stroke-purple-600"
 				>
 					<rect width="18" height="18" x="3" y="4" rx="2" />
 					<path d="M16 2v4" />
@@ -115,10 +132,11 @@ export default function NavFrame() {
 					<NavButton label={"People"} href={""} />
 				</div>
 			</div>
-			<div className="flex flex-row flex-nowrap gap-6">
+			<div className="flex flex-row flex-nowrap items-center gap-6">
 				{searchSymbol}
 				{settingsSymbol}
 				{notificationSymbol}
+				<ProfileFrame />
 			</div>
 		</div>
 	);
