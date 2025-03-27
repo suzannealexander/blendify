c04c9d63072ee912d552bf1e7b1528b764722874
ray <rayvantsahni@gmail.com>
2025-03-27 01:59:25 -0400

    Update models.py for Django

--- backend/chore_tracker/models.py
+++ backend/chore_tracker/models.py
@@ -1,3 +1,68 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=40)
+    username = models.CharField(max_length=40)
+    password = models.CharField(max_length=162)
+    email = models.EmailField(max_length=60)
+    photo_url = models.CharField(max_length=60, default='None')
+
+    def __str__(self):
+        return self.username
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=60)
+    status = models.CharField(max_length=30)
+    expiration = models.DateTimeField(null=True, blank=True)
+    timezone = models.CharField(max_length=30)
+
+    # Relationships
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owned_groups')
+    members = models.ManyToManyField(User, related_name='joined_groups')
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=60)
+    first_date = models.DateField()
+    first_time = models.TimeField()
+    repeat_every = models.CharField(max_length=40, null=True, blank=True)
+
+    # Relationships
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='events')
+    members = models.ManyToManyField(User, related_name='events')
+
+    def __str__(self):
+        return self.name
+
+
+# class Cost(models.Model):
+#     name = models.CharField(max_length=60)
+#     category = models.CharField(max_length=40, null=True, blank=True)
+#     amount = models.FloatField()
+#
+#     # Relationships
+#     group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='costs')
+#     recipient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='receipts')
+#     senders = models.ManyToManyField(User, related_name='costs')
+#
+#     def __str__(self):
+#         return self.name
+
+
+class Cost(models.Model):
+    name = models.CharField(max_length=60)
+    category = models.CharField(max_length=40, null=True, blank=True)
+    amount = models.FloatField()
+
+    # Relationships
+    payer = models.ForeignKey(User, on_delete=models.CASCADE, related_name='paid_costs')
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, related_name='group_costs')
+    borrower = models.ForeignKey(User, on_delete=models.CASCADE, related_name='borrowed_costs')
+
+    def __str__(self):
+        return self.name
