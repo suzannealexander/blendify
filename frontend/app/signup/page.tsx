5e831a0910f449fea8cf6cd260c6f204b107e269
beatrice <bbrownmulry@gmail.com>
2025-03-28 17:49:25 -0400

    move SignupFrame component outside page.tsx

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,27 +1,7 @@
 import PageLayout from "@/components/PageLayout";
-import SignupForm from "@/components/SignupForm";
+import SignupFrame from "@/components/SignupFrame";
 import Link from "next/link";
 
-function SignupFrame() {
-	return (
-		<div className="m-auto h-full max-w-md space-y-8 py-12">
-			<div className="text-center">
-				<h2 className="mt-6 text-3xl font-extrabold text-gray-900">
-					Create an account
-				</h2>
-			</div>
-			<SignupForm />
-			<div className="text-center">
-				<Link
-					href="/login"
-					className="font-medium text-purple-600 hover:text-purple-500"
-				>
-					Already have an account? Log in
-				</Link>
-			</div>
-		</div>
-	);
-}
 export default function SignupPage() {
 	return (
 		<PageLayout>
