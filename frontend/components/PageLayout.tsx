e9064358384885a272ed5bc9841bbdf01d4e28c9
beatrice <bbrownmulry@gmail.com>
2025-03-28 17:56:49 -0400

    remove outdated import and relax typing on PageLayout to allow nulls

--- frontend/components/PageLayout.tsx
+++ frontend/components/PageLayout.tsx
@@ -1,8 +1,11 @@
-import ContentFrame from "@/components/ContentFrame";
 import NavFrame from "@/components/NavFrame";
 import { JSX } from "react";
 
-export default function PageLayout({ children }: { children: JSX.Element }) {
+export default function PageLayout({
+	children,
+}: {
+	children: JSX.Element | null;
+}) {
 	return (
 		<div className="flex h-[100vh] w-[100vw] flex-col flex-nowrap">
 			<div className="h-max w-full border-b-[1px] border-gray-300 p-4 shadow-sm">
