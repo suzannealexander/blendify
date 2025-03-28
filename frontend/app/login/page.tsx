78872c05e0152d4e21fc8bfe6f52780df9207d29
beatrice <bbrownmulry@gmail.com>
2025-03-28 17:51:09 -0400

    move LoginFrame component outside page.tsx

--- frontend/app/login/page.tsx
+++ frontend/app/login/page.tsx
@@ -1,30 +1,6 @@
-import LoginForm from "@/components/LoginForm";
+import LoginFrame from "@/components/LoginFrame";
 import PageLayout from "@/components/PageLayout";
-import Link from "next/link";
 
-function LoginFrame() {
-	return (
-		<div className="m-auto h-full max-w-md space-y-8 py-12">
-			<div className="text-center">
-				<h2 className="mt-6 text-3xl font-extrabold text-gray-900">
-					Log in to your account
-				</h2>
-				<p className="mt-2 text-sm text-gray-600">
-					Welcome back! Please enter your details.
-				</p>
-			</div>
-			<LoginForm />
-			<div className="text-center">
-				<Link
-					href="/signup"
-					className="font-medium text-purple-600 hover:text-purple-500"
-				>
-					Don't have an account? Sign up
-				</Link>
-			</div>
-		</div>
-	);
-}
 export default function LoginPage() {
 	return (
 		<PageLayout>
