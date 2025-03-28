bde5a47a5c889c25c2910d3f4b700fedb4174654
beatrice <bbrownmulry@gmail.com>
2025-03-28 17:51:15 -0400

    remove outdated import

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,6 +1,5 @@
 import PageLayout from "@/components/PageLayout";
 import SignupFrame from "@/components/SignupFrame";
-import Link from "next/link";
 
 export default function SignupPage() {
 	return (
