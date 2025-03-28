c21571efbf38f993b719a93c1e0b9db1fce292e4
beatrice <bbrownmulry@gmail.com>
2025-03-28 19:40:04 -0400

    differentiate between the current date, displayed date, and target date for display purposes on the frontend

--- frontend/components/CalendarFrame.tsx
+++ frontend/components/CalendarFrame.tsx
@@ -1,33 +1,50 @@
 "use client";
 
-import dayjs, { Dayjs } from "dayjs";
+import dayjs, { Dayjs, months } from "dayjs";
 import weekday from "dayjs/plugin/weekday";
 import localeData from "dayjs/plugin/localeData";
 import { useState } from "react";
+import { GroupDisplayData } from "@/schema";
 
 dayjs.extend(weekday);
 dayjs.extend(localeData);
 
-export default function CalendarFrame() {
-	const today: Dayjs = dayjs();
-	const [currentDate, setCurrentDate] = useState<Dayjs>(today);
-
+export default function CalendarFrame({
+	groupData,
+	currentDate,
+	displayDate,
+	setDisplayDate,
+	targetDate,
+	setTargetDate,
+}: {
+	groupData: GroupDisplayData;
+	currentDate: Dayjs;
+	displayDate: Dayjs;
+	setDisplayDate: CallableFunction;
+	targetDate: Dayjs;
+	setTargetDate: CallableFunction;
+}) {
 	const handlePrevMonth = () => {
-		setCurrentDate(currentDate.subtract(1, "month"));
+		setDisplayDate(displayDate.subtract(1, "month"));
 	};
 
 	const handleNextMonth = () => {
-		setCurrentDate(currentDate.add(1, "month"));
+		setDisplayDate(displayDate.add(1, "month"));
 	};
 
 	return (
 		<div className="h-max w-full rounded-lg border-[1px] border-gray-300 p-4 shadow-sm">
 			<CalendarHeader
-				currentDate={currentDate}
+				currentDate={displayDate}
 				onPrev={handlePrevMonth}
 				onNext={handleNextMonth}
 			/>
-			<CalendarGrid currentDate={currentDate} today={today} />
+			<CalendarGrid
+				displayDate={displayDate}
+				currentDate={currentDate}
+				targetDate={targetDate}
+				setTargetDate={setTargetDate}
+			/>
 		</div>
 	);
 }
@@ -81,13 +98,20 @@ function CalendarHeader({ currentDate, onPrev, onNext }: CalendarHeaderProps) {
 }
 
 type CalendarGridProps = {
+	displayDate: Dayjs;
 	currentDate: Dayjs;
-	today: Dayjs;
+	targetDate: Dayjs;
+	setTargetDate: CallableFunction;
 };
 
-function CalendarGrid({ currentDate, today }: CalendarGridProps) {
-	const startOfMonth = currentDate.startOf("month");
-	const endOfMonth = currentDate.endOf("month");
+function CalendarGrid({
+	displayDate,
+	currentDate,
+	targetDate,
+	setTargetDate,
+}: CalendarGridProps) {
+	const startOfMonth = displayDate.startOf("month");
+	const endOfMonth = displayDate.endOf("month");
 	const startDay = startOfMonth.weekday();
 	const daysInMonth = endOfMonth.date();
 
@@ -122,8 +146,10 @@ function CalendarGrid({ currentDate, today }: CalendarGridProps) {
 						<CalendarDay
 							key={`${i}-${j}`}
 							day={day}
-							today={today}
 							currentDate={currentDate}
+							displayDate={displayDate}
+							targetDate={targetDate}
+							setTargetDate={setTargetDate}
 						/>
 					)),
 				)}
@@ -134,27 +160,63 @@ function CalendarGrid({ currentDate, today }: CalendarGridProps) {
 
 type CalendarDayProps = {
 	day: number | null;
-	today: Dayjs;
 	currentDate: Dayjs;
+	displayDate: Dayjs;
+	targetDate: Dayjs;
+	setTargetDate: CallableFunction;
 };
 
-function CalendarDay({ day, today, currentDate }: CalendarDayProps) {
-	const isToday =
-		day !== null &&
-		today.date() === day &&
-		today.month() === currentDate.month() &&
-		today.year() === currentDate.year();
+function CalendarDay({
+	day,
+	currentDate,
+	displayDate,
+	targetDate,
+	setTargetDate,
+}: CalendarDayProps) {
+	const handleClick = () =>
+		day
+			? setTargetDate(
+					dayjs()
+						.set("date", day)
+						.set("month", displayDate.month())
+						.set("year", displayDate.year()),
+				)
+			: null;
+
+	const isCurrentDate: boolean =
+		currentDate.date() === day &&
+		currentDate.month() === displayDate.month() &&
+		currentDate.year() === displayDate.year();
+
+	const isTargetDate: boolean =
+		targetDate.date() === day &&
+		targetDate.month() === displayDate.month() &&
+		targetDate.year() === displayDate.year();
+
+	var classElements: string =
+		"flex h-16 items-start justify-start rounded-md p-2 text-left text-sm font-semibold";
+
+	// don't consider anything else if the day is null
+	if (day === null) {
+		classElements = classElements + " bg-gray-50";
+	} else {
+		// style the current date differently
+		if (isCurrentDate) {
+			classElements =
+				classElements +
+				" bg-purple-200 hover:bg-purple-300 text-purple-900";
+		} else {
+			classElements = classElements + " bg-gray-100 hover:bg-gray-200";
+		}
+
+		// style the target date differently
+		if (isTargetDate) {
+			classElements = classElements + " ring-3 ring-purple-600";
+		}
+	}
 
 	return (
-		<div
-			className={`flex h-16 items-start justify-start rounded-md p-2 text-left text-sm font-semibold ${
-				day
-					? isToday
-						? "bg-purple-200 text-purple-800 hover:bg-purple-300"
-						: "bg-gray-100 hover:bg-gray-200"
-					: ""
-			}`}
-		>
+		<div className={classElements} onClick={handleClick}>
 			{day && <span>{day}</span>}
 		</div>
 	);
--- frontend/components/HomeFrame.tsx
+++ frontend/components/HomeFrame.tsx
@@ -1,64 +1,114 @@
 "use client";
 
-import { Dispatch, SetStateAction, use, useState } from "react";
+import { Dispatch, JSX, SetStateAction, use, useState } from "react";
 import CalendarFrame from "@/components/CalendarFrame";
 import ToDoFrame from "@/components/ToDoFrame";
 import { GroupDisplayData, UserDisplayData } from "@/schema";
+import dayjs, { Dayjs } from "dayjs";
+
+// function getGroupById(userData: UserDisplayData, targetGroupId: number) {
+// 	return (
+// 		userData.groups.find(
+// 			(group: GroupDisplayData) => group.id === targetGroupId,
+// 		) || null
+// 	);
+// }
 
 export default function HomeFrame({ userData }: { userData: UserDisplayData }) {
+	// defaults to null if the user is not in any groups, otherwise defaults to the first
+	const [currentGroup, setCurrentGroup] = useState<GroupDisplayData | null>(
+		userData.groups.length > 0 ? userData.groups[0] : null,
+	);
+	// current date
+	const currentDate: Dayjs = dayjs();
+
+	// date displayed on the calendar
+	const [displayDate, setDisplayDate] = useState<Dayjs>(currentDate);
+
+	// date targeted on the calendar and shown on the todo list
+	const [targetDate, setTargetDate] = useState<Dayjs>(currentDate);
+
+	const homeContents =
+		currentGroup !== null ? (
+			<div className="flex h-full w-full flex-row flex-nowrap gap-12 p-10">
+				<ToDoFrame groupData={currentGroup} />
+				<CalendarFrame
+					groupData={currentGroup}
+					currentDate={currentDate}
+					displayDate={displayDate}
+					setDisplayDate={setDisplayDate}
+					targetDate={targetDate}
+					setTargetDate={setTargetDate}
+				/>
+			</div>
+		) : (
+			<div className="">Please select a group</div>
+		);
+
 	return (
 		<div className="m-auto h-max w-full max-w-5xl">
-			<GroupSelector groups={userData.groups} />
-			<div className="flex h-full w-full flex-row flex-nowrap gap-12 p-10">
-				<ToDoFrame />
-				<CalendarFrame />
-			</div>
+			<GroupSelector
+				groups={userData.groups}
+				setGroupCallback={setCurrentGroup}
+				currentGroupId={currentGroup?.id}
+			/>
+			{homeContents}
 		</div>
 	);
 }
 
 function GroupSelectorButton({
-	label,
+	group,
 	currentGroupId,
-	nextGroupId,
-	setGroupId,
+	setGroupCallback,
 }: {
-	label: string;
-	currentGroupId: number | null;
-	nextGroupId: number;
-	setGroupId: Dispatch<SetStateAction<number | null>>;
+	group: GroupDisplayData;
+	currentGroupId: number | undefined;
+	setGroupCallback: CallableFunction;
 }) {
-	const handleClick = () => setGroupId(nextGroupId);
+	const handleClick = () => setGroupCallback(group);
 	// make an explicit new version of this component to create new groups
 	// should it redirect the user or just set a diff page state?
-
-	return (
-		<div
-			className="cursor-pointer rounded p-2 text-sm hover:bg-gray-50"
-			onClick={handleClick}
-		>
-			{label}
-		</div>
-	);
+	const isSelected: boolean = group.id === currentGroupId;
+	if (isSelected) {
+		return (
+			<div
+				className="cursor-pointer rounded bg-purple-100 p-2 text-sm hover:bg-purple-200"
+				onClick={handleClick}
+			>
+				{group.name}
+			</div>
+		);
+	} else {
+		return (
+			<div
+				className="cursor-pointer rounded p-2 text-sm hover:bg-gray-50"
+				onClick={handleClick}
+			>
+				{group.name}
+			</div>
+		);
+	}
 }
 
-function GroupSelector({ groups }: { groups: GroupDisplayData[] }) {
-	const [currentGroup, setCurrentGroup] = useState<number | null>(null);
+function GroupSelector({
+	groups,
+	currentGroupId,
+	setGroupCallback,
+}: {
+	groups: GroupDisplayData[];
+	currentGroupId: number | undefined;
+	setGroupCallback: CallableFunction;
+}) {
 	return (
 		<div className="flex flex-row flex-nowrap items-center justify-center gap-8 border-b-[1px] border-gray-200 p-4">
-			<GroupSelectorButton
-				label={"+ New Group"}
-				currentGroupId={currentGroup}
-				nextGroupId={0}
-				setGroupId={setCurrentGroup}
-			/>
+			{/* <NewGroupButton /> */}
 			{groups.map((group: GroupDisplayData) => (
 				<GroupSelectorButton
 					key={group.id}
-					label={group.name}
-					currentGroupId={currentGroup}
-					nextGroupId={group.id}
-					setGroupId={setCurrentGroup}
+					group={group}
+					currentGroupId={currentGroupId}
+					setGroupCallback={setGroupCallback}
 				/>
 			))}
 		</div>
