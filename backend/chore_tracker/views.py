de81c8c89b37ffbe92db67c44dab8a3cb3e2ecc6
Lance Kennedy <lancek23@gmail.com>
2025-03-28 22:33:37 -0400

    Added API route for adding event to household

--- backend/chore_tracker/views.py
+++ backend/chore_tracker/views.py
@@ -7,37 +7,40 @@ from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from schema.alchemy_models import EventAlchemy
 import datetime
+import json
+from json import JSONDecodeError
+from .models import Event
 
 
 class RegisterUser(APIView):
-    """ User Registration """
+    """User Registration"""
 
     def post(self, request):
-        username = request.data.get('username')
-        email = request.data.get('email')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        email = request.data.get("email")
+        password = request.data.get("password")
 
         if User.objects.filter(username=username).exists():
-            return Response({'error': 'Username already exists'}, status=400)
+            return Response({"error": "Username already exists"}, status=400)
 
         if User.objects.filter(email=email).exists():
-            return Response({'error': 'Email already in use'}, status=400)
+            return Response({"error": "Email already in use"}, status=400)
 
         try:
             User.objects.create_user(username=username, email=email, password=password)
         except ValidationError as e:
-            return Response({'error': str(e)}, status=400)
+            return Response({"error": str(e)}, status=400)
 
-        return Response({'message': 'User registered successfully'}, status=201)
+        return Response({"message": "User registered successfully"}, status=201)
 
 
 class LoginView(APIView):
-    """ User Login with JWT in Cookies """
+    """User Login with JWT in Cookies"""
+
     def post(self, request):
-        username = request.data.get('username')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        password = request.data.get("password")
 
         # case-insensitive username check
         user = User.objects.filter(username__iexact=username).first()
@@ -46,49 +49,60 @@ class LoginView(APIView):
 
             login(request, user)
             refresh = RefreshToken.for_user(user)
-            response = Response({
-                'message': 'Login successful',
-            })
+            response = Response(
+                {
+                    "message": "Login successful",
+                }
+            )
 
             response.set_cookie(
-                key='access_token',
+                key="access_token",
                 value=str(refresh.access_token),
                 httponly=True,
                 secure=False,
-                samesite='Lax'
+                samesite="Lax",
             )
             response.set_cookie(
-                key='refresh_token',
+                key="refresh_token",
                 value=str(refresh),
                 httponly=True,
                 secure=False,
-                samesite='Lax'
+                samesite="Lax",
             )
 
             return response
         else:
-            return Response({'error': 'Invalid credentials'}, status=401)
+            return Response({"error": "Invalid credentials"}, status=401)
 
 
 def add_event_to_household(request):
-    # TODO: Ask frontend how request JSON will look
-    # TODO: Add verification of expected data
 
     if request.method == "POST":
-        event = EventAlchemy(
-            id=request["event_id"],
-            name=request["event_name"],
-            first_date=datetime.date.today(),
-            first_time=datetime.datetime.now().time(),
-            repeat_every=request["repeat_every"],
-            group_id=request["group_id"],
-            # TODO: Query for group name, members, and member ids, add to Event
-        )
+        try:
+            data = json.loads(request.body)
+            event = Event.objects.create(
+                id=data.get("id"),
+                name=data.get("event_name"),
+                # TODO: Determine date format
+                first_date=datetime.strptime(
+                    data.get("date"), "%Y-%m-%d %H:%M:%S"
+                ).date(),
+                first_time=datetime.strptime(
+                    data.get("date"), "%Y-%m-%d %H:%M:%S"
+                ).time(),
+                # TODO: Add this in, for now its not in interface in schema.ts
+                repeat_every="",
+                group=data.get("group_id"),
+                members=data.get("members"),
+            )
 
-        # TODO: Add Event to Group/Household
+            return JsonResponse({"success": True, "message": ""}, status=200)
 
-        # TODO: Ask what frontend needs on return
-        return JsonResponse({"success": True}, status=200)
+        except JSONDecodeError:
+            return JsonResponse(
+                {"success": False, "message": "Invalid JSON in request"}, status=400
+            )
 
-    return JsonResponse({"success": False}, status=405)
-
+    return JsonResponse(
+        {"success": False, "message": "Expected POST method"}, status=405
+    )
